3d899fe33e1a65ebf36b08e6a962ceadd3de8706
Alex Flávio <alexflavio.dev@gmail.com>
2024-12-19 15:42:30 -0300

    feat: add create-question and fetch-recent-questions controllers
    Implemented controllers to handle question creation and fetch recent questions.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AuthModule } from "./auth/auth.module"
 import { AuthenticateController } from "./controllers/authenticate.controller"
 import { CreateAccountController } from "./controllers/create-account.controller"
 import { CreateQuestionController } from "./controllers/create-question.controller"
+import { FetchRecentQuestionsController } from "./controllers/fetch-recents-questions.controller"
 import { envSchema } from "./env"
 import { PrismaService } from "./prisma/prisma.service"
 @Module({
@@ -17,6 +18,7 @@ import { PrismaService } from "./prisma/prisma.service"
   controllers: [
     CreateAccountController,
     CreateQuestionController,
+    FetchRecentQuestionsController,
     AuthenticateController,
   ],
   providers: [PrismaService],
--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, HttpCode, Post, UseGuards, UsePipes } from "@nestjs/common"
+import { Body, Controller, HttpCode, Post, UseGuards } from "@nestjs/common"
 import { CurrentUser } from "src/auth/current-user-decorator"
 import { JwtAuthGuard } from "src/auth/jwt.auth.guard"
 import { UserPayload } from "src/auth/jwt.strategy"
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe"
 import { PrismaService } from "src/prisma/prisma.service"
 import { z } from "zod"
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
-  slug: z.string().optional(),
   content: z.string(),
 })
 
 type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
 
 @Controller("/questions")
 @UseGuards(JwtAuthGuard)
@@ -20,9 +22,33 @@ export class CreateQuestionController {
 
   @Post()
   @HttpCode(201)
-  @UsePipes()
-  // new ZodValidationPipe(createQuestionBodySchema)
-  async handle(@CurrentUser() user: UserPayload) {
-    return "ok"
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload
+  ) {
+    const { title, content } = body
+    const userId = user.sub
+
+    const slug = this.convertToSlug(title)
+
+    await this.prisma.question.create({
+      data: {
+        content,
+        title,
+        slug: slug,
+        authorId: userId,
+      },
+    })
+
+    return
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/[^\w\s-]/g, "")
+      .replace(/\s+/g, "-")
   }
 }
