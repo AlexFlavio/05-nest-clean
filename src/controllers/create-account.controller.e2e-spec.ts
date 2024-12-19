a53ad4df16a6aa9365deef1b40bd10d248b24bc7
Alex Flávio <alexflavio.dev@gmail.com>
2024-12-19 16:46:44 -0300

    test(e2e): add test for [POST] /accounts

---
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -0,0 +1,38 @@
+import { AppModule } from "@/app.module"
+import { PrismaService } from "@/prisma/prisma.service"
+import { INestApplication } from "@nestjs/common"
+import { Test } from "@nestjs/testing"
+import request from "supertest"
+
+describe("Create Account (E2E)", () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+  test("[POST] /accounts", async () => {
+    const response = await request(app.getHttpServer()).post("/accounts").send({
+      name: "Alex Flavio",
+      email: "alex@teste.com",
+      password: "senha secreta",
+    })
+    expect(response.statusCode).toBe(201)
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        email: "alex@teste.com",
+      },
+    })
+
+    expect(userOnDatabase).toBeTruthy()
+  })
+})
